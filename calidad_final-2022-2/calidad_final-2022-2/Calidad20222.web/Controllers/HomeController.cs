70be642ec90622f3b825c1ba7ffcec174b4515a6
Brandon Rodriguez <89100676+BMRCVIP@users.noreply.github.com>
2022-11-30 23:50:07 -0500

    Subida de Examen
    Se ha corregido y completado la lógica PokerService.cs
    En la misma vista index de agrego para permitir ingresar 5 cartas y mostrarme GetJugada
    Se genero 20 pruebas unitarias para PokerService
    Se creo 5 pruebas Selenium

---
+++ calidad_final-2022-2/calidad_final-2022-2/Calidad20222.web/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics;
+using Calidad20222.web.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Calidad20222.web.Models;
+using Calidad20222.web.Services;
+
+namespace Calidad20222.web.Controllers;
+
+public class HomeController : Controller
+{
+    //private readonly ILogger<HomeController> _logger;
+
+    public HomeController()
+    {
+       
+    }
+
+    /*public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }*/
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new List<Carta>());
+    }
+    
+   [HttpPost]
+    public IActionResult Generar()
+    {
+        var service = new PokerService();
+        var cartas = GeneradorMano.Generar();
+        ViewBag.Jugada = service.GetJugada(cartas);
+        return View("Index", cartas);
+    }
+
+    [HttpPost]
+    public IActionResult Generar_1
+        (String Primera, String Segunda, String Tercera, 
+        String Cuarta, String Quinta, String Sexta, 
+        String PrimerPalo, String SegundoPalo, String TercerPalo, 
+        String CuartoPPalo, String QuintoPalo)
+    {
+        List<Carta> lista = new List<Carta>()
+        {
+            new Carta
+            {
+                Numero = int.Parse(Primera),
+                Palo = PrimerPalo
+            },
+            new Carta
+            {
+                Numero = int.Parse(Segunda),
+                Palo = SegundoPalo
+            },
+            new Carta
+            {
+                Numero = int.Parse(Tercera),
+                Palo = TercerPalo
+            },
+            new Carta
+            {
+                Numero = int.Parse(Cuarta),
+                Palo =CuartoPPalo
+            },
+            new Carta
+            {
+                Numero = int.Parse(Quinta),
+                Palo = QuintoPalo
+            }
+        };
+        var service = new PokerService();
+        //var cartas = GeneradorMano.Generar();
+        ViewBag.Jugada = service.GetJugada(lista);
+        return View("Index", lista);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+    }
+}
---
+++ calidad_final-2022-2/calidad_final-2022-2/TestExamenFinal/ControladorTest.cs
@@ -0,0 +1,653 @@
+﻿using Calidad20222.web.Controllers;
+using Calidad20222.web.Helper;
+using Calidad20222.web.Models;
+using Calidad20222.web.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExamenFinal
+{
+    public class ControladorTest
+    {
+        PokerService pokerService;
+
+        //Testing para carta Mayor
+        [Test]
+        public void CartaMayorTest01()
+        {
+            List<Carta> lista = new List<Carta>() { 
+                new Carta{
+                    Numero=10,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=1,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=9,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=4,
+                    Palo ="COCO"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("CARTA MAYOR",result);
+        }
+
+        [Test]
+        public void CartaMayorTest01_1()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=3,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=1,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=10,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=9,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=5,
+                    Palo ="COCO"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("CARTA MAYOR", result);
+        }
+
+
+        //Testing para carta doble
+
+        [Test]
+        public void CartaDobleTest02()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=13,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=2,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=8,
+                    Palo ="COCO"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("DOBLE", result);
+        }
+
+        [Test]
+        public void CartaDobleTest02_1()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=2,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=2,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=12,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=8,
+                    Palo ="COCO"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("DOBLE", result);
+        }
+
+
+        //Testing para carta doble
+
+        [Test]
+        public void CartaDobleParejaTest03()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=4,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=4,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=8,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=8,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=1,
+                    Palo ="COCO"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("DOBLE PAREJA", result);
+        }
+
+        [Test]
+        public void CartaDobleParejaTest03_1()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=1,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=1,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=8,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="COCO"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("DOBLE PAREJA", result);
+        }
+
+
+        //Testing para carta TRIO
+
+        [Test]
+        public void CartaTrioTest04()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=13,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=8,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=1,
+                    Palo ="CORAZON"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("TRIO", result);
+        }
+
+        [Test]
+        public void CartaTrioTest04_1()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=2,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=2,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=8,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=2,
+                    Palo ="CORAZON"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("TRIO", result);
+        }
+
+
+        //Testing para carta Escalera
+
+        [Test]
+        public void CartaEscaleraTest05()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=3,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=4,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=5,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=7,
+                    Palo ="CORAZON"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("ESCALERA", result);
+        }
+
+
+        [Test]
+        public void CartaEscaleraTest05_1()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=8,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=9,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=10,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=11,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=12,
+                    Palo ="CORAZON"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("ESCALERA", result);
+        }
+
+
+        //Testing para carta Color
+        [Test]
+        public void CartaColorTest06()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=13,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=4,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=2,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=1,
+                    Palo ="CORAZON"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("COLOR", result);
+        }
+
+        [Test]
+        public void CartaColorTest06_1()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=5,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=2,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=1,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=10,
+                    Palo ="TREBOL"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("COLOR", result);
+        }
+
+
+        //Testing para carta Full
+        [Test]
+        public void CartaFullTest07()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=1,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=1,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=1,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="COCO"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("FULL", result);
+        }
+
+        [Test]
+        public void CartaFullTest07_1()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=5,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=5,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="COCO"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("FULL", result);
+        }
+
+
+
+        //Testing para carta POKER
+
+        [Test]
+        public void CartaPokerTest08()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=2,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=2,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=2,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=2,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="COCO"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("POKER", result);
+        }
+
+        [Test]
+        public void CartaPokerTest08_1()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=2,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="TREBOL"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="COCO"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("POKER", result);
+        }
+
+
+
+        //Testing para carta EscaleraColor
+        [Test]
+        public void CartaEscaleraColorTest09()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=7,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=8,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=9,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=10,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=11,
+                    Palo ="ESPADA"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("ESCALERA DE COLOR", result);
+        }
+
+        [Test]
+        public void CartaEscaleraColorTest09_1()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=3,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=4,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=5,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=6,
+                    Palo ="COCO"
+                },
+                new Carta{
+                    Numero=7,
+                    Palo ="COCO"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("ESCALERA DE COLOR", result);
+        }
+
+
+        //Testing para carta EscaleraReal
+
+        [Test]
+        public void CartaEscaleraRealTest10()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=1,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=12,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=11,
+                    Palo ="ESPADA"
+                },
+                new Carta{
+                    Numero=10,
+                    Palo ="ESPADA"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("ESCALERA REAL", result);
+        }
+
+
+        [Test]
+        public void CartaEscaleraRealTest10_1()
+        {
+            List<Carta> lista = new List<Carta>() {
+                new Carta{
+                    Numero=1,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=13,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=12,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=11,
+                    Palo ="CORAZON"
+                },
+                new Carta{
+                    Numero=10,
+                    Palo ="CORAZON"
+                },
+            };
+            pokerService = new PokerService();
+            var result = pokerService.GetJugada(lista);
+            Assert.AreEqual("ESCALERA REAL", result);
+        }
+
+
+
+
+
+
+    }
+}
